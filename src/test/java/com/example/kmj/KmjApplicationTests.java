cc4d24c8a1c79fde0346fe0c2d4ceac7387f2d82
reokkamy <quinate@kakao.com>
2025-07-20 22:34:11 +0900

    first commit

---
+++ src/test/java/com/example/kmj/KmjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kmj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
