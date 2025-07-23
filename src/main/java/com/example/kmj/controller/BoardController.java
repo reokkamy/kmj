f5a7e1f85d088108ec4081f1f90ec1c2b07c150e
mj <quinate@kakao.com>
2025-07-23 17:38:15 +0900

    7/21 프로젝트 비속어 필터 테스트

--- src/main/java/com/example/kmj/controller/BoardController.java
+++ src/main/java/com/example/kmj/controller/BoardController.java
@@ -1,11 +1,11 @@
 package com.example.kmj.controller;
 
 
-import com.example.kmj.repository.BoardRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
+
 
 @Controller
 @RequiredArgsConstructor
