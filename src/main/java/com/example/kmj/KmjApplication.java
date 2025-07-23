e9d5e90d9145fc972e2d443911a5c018c06ae2c5
mj <quinate@kakao.com>
2025-07-23 14:04:17 +0900

    7/21 프로젝트 비속어 필터 테스트

---
+++ src/main/java/com/example/kmj/KmjApplication.java
@@ -0,0 +1,11 @@
+package com.example.kmj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmjApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KmjApplication.class, args);
+    }
+}
